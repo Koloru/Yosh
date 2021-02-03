9f4f2fd1a82b2dd51e5828791e504ebff035e937
Color <sos.llyr@gmail.com>
2021-02-03 17:31:43 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-// document.body.addEventListener('click', ()=>{
-//   window.location.replace("https://syko.cc/rushiabruh.mp4")
-// })
+document.body.addEventListener('click', ()=>{
+  window.location.replace("https://www.youtube.com/watch?v=e05NDRsxL5Y")
+})
+
+const x = document.querySelector('video')
+function playVideo() {
+  x.play();
+  x.loop = true;
+}
