99c26d9ec324a4f51941d5f2ecbfab4368377e6b
Color <sos.llyr@gmail.com>
2022-09-09 20:48:45 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-document.body.addEventListener('click', ()=>{
-  window.location.replace("https://www.syko.cc/rushiabruh.mp4")
-})
-
 const x = document.querySelector('video')
 function playVideo() {
   x.play();
